5eb0c122ff721d42e9fefe14bdb6ea0050fb14fd
Moria El Akaya <makaya1@my.westga.edu>
2024-12-03 18:43:29 -0500

    Update the player class -Added a live and inventory field - Changed name for playerhitPoints to health

--- code/src/edu/westga/cs3211/text_adventure_game/model/Player.java
+++ code/src/edu/westga/cs3211/text_adventure_game/model/Player.java
@@ -5,39 +5,92 @@ package edu.westga.cs3211.text_adventure_game.model;
  * 
  * Holds all the information related to the player.
  * 
- * @author Shawn Bretthauer
+ * @author Shawn Bretthauer and MoriaEL
  * @version Fall 2024
  */
 public class Player {
 
-	private int playerHitPoints;
+    private int lives;
+    private int health;
+    private Inventory inventory;
 
-	/**
-	 * Constructor for a new Player object
-	 */
-	public Player() {
-		this.playerHitPoints = 10;
-	}
+    /**
+     * Constructor for a new Player object
+     */
+    public Player() {
+        this.lives = 3;
+        this.health = 10;
+        this.inventory = new Inventory();
+    }
 
-	/**
-	 * Reduces the Player's hit points by the damage value of the hazard.
-	 * 
-	 * @param damage the damage value of the hazard
-	 */
-	public void reducePlayerHitPoint(int damage) {
-		this.playerHitPoints = this.playerHitPoints - damage;
+    /**
+     * Reduces the Player's health by the damage value. 
+     * 
+     * @param damage the damage value
+     */
+    public void reduceHealth(int damage) {
+        this.health -= damage;
+        if (this.health < 0) {
+            this.health = 0;
+        }
+    }
 
-		if (this.playerHitPoints < 0) {
-			this.playerHitPoints = 0;
-		}
-	}
+    /**
+     * Returns the current health of the player.
+     * 
+     * @return health
+     */
+    public int getHealth() {
+        return this.health;
+    }
 
-	/**
-	 * Returns the current HP of the player.
-	 * 
-	 * @return playerHitPoints
-	 */
-	public int getPlayerHitPoints() {
-		return this.playerHitPoints;
-	}
+    /**
+     * Checks if the player is dead.
+     * 
+     * @return true if health <= 0
+     */
+    public boolean isDead() {
+        return this.health <= 0;
+    }
+
+    /**
+     * Resets the player's health to full.
+     */
+    public void resetHealth() {
+        this.health = 10;
+    }
+
+    /**
+     * Gets the player's inventory.
+     * 
+     * @return inventory
+     */
+    public Inventory getInventory() {
+        return this.inventory;
+    }
+
+    /**
+     * Gets the player's remaining lives.
+     * 
+     * @return lives
+     */
+    public int getLives() {
+        return this.lives;
+    }
+
+    /**
+     * Reduces the player's lives by one.
+     */
+    public void loseLife() {
+        this.lives--;
+    }
+
+    /**
+     * Checks if the player has lives remaining.
+     * 
+     * @return true if lives > 0
+     */
+    public boolean hasLives() {
+        return this.lives > 0;
+    }
 }
